e72be6f9f514de142018bbc9fdebea7597afb563
brandon kiiker <brandon.kiiker6@gmail.com>
2019-06-04 10:41:48 +0300

    Alustus

---
+++ Muusikapood/Muusikapood/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Muusikapood.Startup))]
+namespace Muusikapood
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
